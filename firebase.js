c4a9bff5f6f9e30d4b12e2d814bb8618c9424046
Coffee-Who <barry00000000@gmail.com>
2026-05-06 16:09:28 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
-```javascript
 const firebaseConfig = {
   apiKey: "AIzaSyBhgfPGUgGsWr4VmOUeaEe_cC0RNSx8I7U",
   authDomain: "swtc-portal-6930c.firebaseapp.com",
-  projectId: "swtc-portal-6930c", // <--- 這裡要加逗點
+  projectId: "swtc-portal-6930c",
   messagingSenderId: "447508396321",
   appId: "1:447508396321:web:ee8d1faaa0331590e5e660"
 };
 
+// 初始化 Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
