bbd8abb40460a2010499061fd666c34156640898
Coffee-Who <barry00000000@gmail.com>
2026-05-06 16:05:12 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,6 +2,8 @@ const firebaseConfig = {
   apiKey: "AIzaSyBhgfPGUgGsWr4VmOUeaEe_cC0RNSx8I7U",
   authDomain: "swtc-portal-6930c.firebaseapp.com",
   projectId: "swtc-portal-6930c"
+  messagingSenderId: "447508396321",
+  appId: "1:447508396321:web:ee8d1faaa0331590e5e660"
 };
 
 firebase.initializeApp(firebaseConfig);
