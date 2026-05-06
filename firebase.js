1c015e298cb35ba0b3a818ec91fa1f9baab1623f
Coffee-Who <barry00000000@gmail.com>
2026-05-06 15:22:51 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBhgfPGUgGsWr4VmOUeaEe_cC0RNSx8I7U",
+  authDomain: "swtc-portal-6930c.firebaseapp.com",
+  projectId: "swtc-portal-6930c"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
