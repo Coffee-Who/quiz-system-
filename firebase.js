28b356d45b97889bbcfbf0d061d3c06db0c598bc
Coffee-Who <barry00000000@gmail.com>
2026-05-06 16:06:07 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,8 @@
+```javascript
 const firebaseConfig = {
   apiKey: "AIzaSyBhgfPGUgGsWr4VmOUeaEe_cC0RNSx8I7U",
   authDomain: "swtc-portal-6930c.firebaseapp.com",
-  projectId: "swtc-portal-6930c"
+  projectId: "swtc-portal-6930c", // <--- 這裡要加逗點
   messagingSenderId: "447508396321",
   appId: "1:447508396321:web:ee8d1faaa0331590e5e660"
 };
